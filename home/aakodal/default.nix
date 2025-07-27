8c20e1911227956a857d613c9e3a0c6fb9c840f7
Aakodal <48600424+Aakodal@users.noreply.github.com>
2025-07-27 13:15:16 +0200

    fix scaling issue
    by Pure Dumb Sheer Luck™

--- home/aakodal/default.nix
+++ home/aakodal/default.nix
@@ -21,7 +21,7 @@
     file.".face.icon".source = assets.profilePicture;
 
     sessionVariables = lib.mkIf (osConfig.networking.hostName == "niflheim") {
-      QT_SCALE_FACTOR = 1.125;
+      QT_SCALE_FACTOR = 1;
       ELM_SCALE = 1.125;
       GDK_SCALE = 1.125;
     }; # helpful for scaling apps
--- home/aakodal/hyprland/config/environment.nix
+++ home/aakodal/hyprland/config/environment.nix
@@ -3,8 +3,8 @@ _: {
     env = [
       "XCURSOR_SIZE,32"
       "QT_SCALE_FACTOR,1"
-      "ELM_SCALE,1"
-      "GDK_SCALE,1"
+      "ELM_SCALE,1.125"
+      "GDK_SCALE,1.125"
       "QT_AUTO_SCREEN_SCALE_FACTOR,1"
     ];
   };
